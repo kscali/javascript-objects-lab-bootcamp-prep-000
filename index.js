362134f779230f6a6bfb46affdacfe8136753dab
kosatun@aol.com <kosatun@aol.com>
2018-07-02 01:54:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var recipes = [];
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
+  var newObject = Object.assign({}, obj,{[key] : value});
+  return newObject;
 }
 
 function 
